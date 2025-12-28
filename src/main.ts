5684f68c55cd21e00a3d13d75cb42a9a8ea6c4f4
Michel González <michel.morales@ducter.com.mx>
2025-12-28 11:19:35 -0600

    Initialize Vue 3 project with Vite, ESLint, and Playwright setup. Added configuration files (.editorconfig, .gitattributes, .gitignore, .prettierrc.json, and ESLint config). Created basic project structure including main application files, components, views, and routing. Included testing setup with Vitest and Playwright, along with a README for project guidance.

---
+++ src/main.ts
@@ -0,0 +1,40 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+import ToastService from 'primevue/toastservice'
+import ConfirmationService from 'primevue/confirmationservice'
+import StyleClass from 'primevue/styleclass'
+import Tooltip from 'primevue/tooltip'
+import 'primeicons/primeicons.css'
+
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+        prefix: 'p',
+        darkModeSelector: 'system',
+        cssLayer: false
+    }
+  },
+})
+app.use(createPinia())
+app.use(router)
+
+app.use(ToastService)
+app.use(ConfirmationService)
+
+// Register directives
+app.directive('styleclass', StyleClass)
+app.directive('tooltip', Tooltip)
+
+
+app.mount('#app')
